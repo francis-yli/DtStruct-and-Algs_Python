e3ce0bd9d6d3ded9e49097323b3022a47679dfb5
yli97 <yli4@oberlin.edu>
2021-02-08 22:07:20 +0800

    initial commit

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -4,6 +4,9 @@ def array_test():
     # create an array
     ar = [e, d, g, a, p]
 
+    print("The current array is:", end = '')
+    for i in ar:
+        print(i, end = "")
 
 def main():
     array_test()
